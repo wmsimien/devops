a7a89434c4dcc6aabf064ddb1874cee74cfc5376
wmavery <wanda.avery@att.net>
2024-08-28 08:16:52 -0500

    initial commit

---
+++ src/test/java/com/averysimien/mydemoapp/MydemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.averysimien.mydemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
